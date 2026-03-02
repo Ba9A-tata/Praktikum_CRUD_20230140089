fcae25e17048ac153a338d246488eb143cb5040e
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-02 10:14:09 +0700

    menambahkan kode di file UserRepository

--- src/main/java/com/example/praktium1/repository/UserRepository.java
+++ src/main/java/com/example/praktium1/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.praktium1.repository;
 
-public interface UserRepository {
+import com.example.praktium1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
